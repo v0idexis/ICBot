5a0df5cb82c99d9f123fa06a085aa8db47b2b680
Harsh <trivediharsh49@gmail.com>
2021-10-02 22:13:30 +0530

    added feature

--- Features/stock.js
+++ Features/stock.js
@@ -1,12 +1,16 @@
 const yahooStockPrices = require("yahoo-stock-prices");
 const daaa = async (sto) => {
-	const ganu = await yahooStockPrices.getCurrentData(`${sto}`);//sto=args[0]
-	console.log(ganu);
-	var s = `*STOCK* :- _${sto}_
-*Currency* :- _${ganu.currency}_                   
-*Price*:- _${ganu.price}_`;
+	var s='';
+ await yahooStockPrices.getCurrentData(`${sto}`).then((res)=>{
+		console.log(res);
+	 s = `*STOCK* :- _${sto}_
+*Currency* :- _${res.currency}_                   
+*Price*:- _${res.price}_`;
+	}).catch((err)=>{
+		s='Not Found';
+	});
 	return s;
-}; // { currency: 'USD', price: 132.05 }
+}; 
 module.exports={
 	daaa
 }
