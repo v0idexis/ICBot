5c151d5a31f335ae166c622309502f6aad3774b0
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2021-10-14 19:23:08 +0530

    Updates (#3)
    * Added Command's configuration
    
    * Added more Features
    
    * updated & added missing dependencies
    
    Co-authored-by: Harsh.Trivedi <trivediharsh49@gmail.com>

--- Features/weather.js
+++ Features/weather.js
@@ -1,5 +1,18 @@
 const axios = require('axios')
 
+const {WAConnection} = require('@adiwajshing/baileys')
+const conn = new WAConnection();
+let weather = async (args)=>{
+
+
+if(!args[0]) throw " please provide place or location name"
+console.log('AHOOOO') 
+console.log('lol'+`https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
+  try{
+
+        const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
+
+
 
 
 
@@ -11,6 +24,7 @@ if(!args[0]) throw " please provide place or location name"
     try{
 
         const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
+
         const res = await response
         
         const name = res.data.name
@@ -21,6 +35,20 @@ if(!args[0]) throw " please provide place or location name"
         const Maximum_Temperature= res.data.main.temp_max + '°C'
         const Humidity= res.data.main.humidity + '%'
         const Wind= res.data.wind.speed + 'km/h'
+
+        // console.log(Country)
+  const wea = `
+  🌸 Place: ${name}\n💮 Country: ${Country}\n🌈 Weather: ${Weather}\n🎋 Temperature: ${Temperature}\n💠 Minimum Temperature: ${Minimum_Temperature}\n📛 Maximum Temperature: ${Maximum_Temperature}\n💦 Humidity: ${Humidity}\n🎐 Wind: ${Wind}
+  `
+//    console.log('the weather is :'+wea)
+    return wea
+  
+//   conn.sendMessage(from,`
+//         🌸 Place: ${name}\n💮 Country: ${Country}\n🌈 Weather: ${Weather}\n🎋 Temperature: ${Temperature}\n💠 Minimum Temperature: ${Minimum_Temperature}\n📛 Maximum Temperature: ${Maximum_Temperature}\n💦 Humidity: ${Humidity}\n🎐 Wind: ${Wind}
+//         `.trim())
+    }catch(e){
+return 'Error location not found!!!'
+
         
 
         conn.reply(m.chat,`
@@ -28,6 +56,7 @@ if(!args[0]) throw " please provide place or location name"
         `.trim(),m)
     }catch(e){
 throw 'location not found' 
+
 console.log(e)
 
     }
@@ -37,8 +66,14 @@ console.log(e)
 
 }
 
+module.exports = weather
+
+
+}
+
 handler.help = ['weather']
 handler.tags = ['internet']
 handler.command = /^(weather|wthr)$/i
 
 module.exports = handler
+
