95e9575af025f32c48a25de9b57931c869747909
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-10-18 19:09:43 +0530

    Add files via upload

---
+++ Features/HELP.js
@@ -0,0 +1,15 @@
+module.exports.help = async ()=>{
+    const str = ` ⚖️IC BOT⚖️
+    💵Crypto
+    💶Crypto_mmi
+    💠Stock
+    🌐Stock_mmi
+    🔰Volatility
+    🥇Gold
+    🥈 Silver
+    📜Exchange rate
+    🌦️Weather
+    💲Currency codes
+    📰News`;
+    return str
+}
