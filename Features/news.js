ed3ea24ca5fc93a42fc1d59b1915c93f8443371a
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2021-11-02 22:37:23 +0530

    Add files via upload

---
+++ Features/news.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const api = 'https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=a7eac679277542249131f57f759e5eed'
+
+const getnews = async() =>{
+    try {
+        const res = await axios.get(api)
+        const data = await res.data
+        const article = await res.data.articles[1]
+        const source = await article.source.name
+        const title = await article.title
+        const url = await article.url
+        const img = await article.urlToImage
+        const publised = await article.publisedAt
+        // console.log(article)        
+        let news = `${title}
+        link to news : ${url}
+        published at : ${publised}`
+        return [news,img]
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+getnews();
+module.exports = {getnews};
