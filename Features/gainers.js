b4d583fb620595257de26be100922a9246aa7066
sauravadiyodi <saurav.adiyodi@gmail.com>
2021-12-03 09:54:50 +0400

    Gainers, Losers refinement
    Updated gainers and losers and fixed some minor errors

--- Features/gainers.js
+++ Features/gainers.js
@@ -1,26 +1,32 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const getgainers = async() =>{
-    try {
-    const Data = await  axios.get('https://www1.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json')
-        .then((response)=>{return response.data});
-        const G = Data.data;
-        let text =[]
-        for(let i = 0;i<G.length;i++){
-            const array = G[i];
-            const lastp = Number(array.ltp.replace(/\,/g,''));
-            const previousprice = Number(array.previousPrice.replace(/\,/g,''));
-            var change = lastp - previousprice;
-            change = change.toFixed(2);
-            let message = `📛Name : *${array.symbol}*\n🔖change : *${array.netPrice}%*\n🏷Change : *${change}}*`
-            text.push(message)
-        }
-        const msg = text.join('\n\n')
-        return msg
-
-    } catch (error) {
-        console.log(error);
-        return 'sorry something went wrong'
+const getgainers = async () => {
+  try {
+    const Data = await axios
+      .get(
+        "https://www1.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json"
+      )
+      .then((response) => {
+        return response.data;
+      });
+    const G = Data.data;
+    let text = [];
+    text.push("*Today's Gainers (NSE)* 📈⬆️");
+    for (let i = 0; i < G.length; i++) {
+      const array = G[i];
+      const lastp = Number(array.ltp.replace(/\,/g, ""));
+      const previousprice = Number(array.previousPrice.replace(/\,/g, ""));
+      var changev = lastp - previousprice;
+      changev = changev.toFixed(2);
+      let message = `📛Name : *${array.symbol}*\n🔺Change : *${changev}* *(${array.netPrice}%)*`;
+      text.push(message);
     }
-}
+    const msg = text.join("\n\n");
+    return msg;
+  } catch (error) {
+    console.log(error);
+    return "Something went wrong, please try again later.";
+  }
+};
+
 module.exports = getgainers;
