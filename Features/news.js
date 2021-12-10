e77a5833dc2a69eba01e33bd6cd47bf2c831fe6b
sauravadiyodi <saurav.adiyodi@gmail.com>
2021-12-10 17:56:55 +0400

    Update news.js
    added backup image

--- Features/news.js
+++ Features/news.js
@@ -8,12 +8,12 @@ const getnews = async () => {
     const res = await axios.get(api);
     const data = await res.data;
 
-    const article1 = await res.data.articles[1];
-    const img = await article1.urlToImage;
+    const article1 = await res.data.articles[0];
+    const img = await article1.urlToImage.error ? "https://www.ridetraffix.com/wp-content/uploads/2019/09/TopTen.jpg" : article1.urlToImage;
 
     let news = ``;
 
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 0; i <= 9; i++) {
       var article = await res.data.articles[i];
       var a_title = await article.title;
       var t_url = await article.url;
@@ -24,10 +24,11 @@ const getnews = async () => {
         news += `${a_title}\nlink : ${a_url}`;
       }
     }
+
     return [news, img];
   } catch (error) {
     console.error(error);
   }
 };
-getnews();
+
 module.exports = { getnews };
