5a8c4ce50c30cc74bd4ee284d38af19d25f0caaa
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2022-01-15 09:27:28 +0530

    Create getjids.js

---
+++ commands/getjids.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+class command {
+    constructor(){
+this.command = 'grpjids' // command goes here
+this.aliases = ['gj']
+    }
+    run = async(m)=>{
+     const jids = fs.readFileSync('../grpjids.json');
+
+       m.reply(JSON.stringify(JSON.parse(jids)))
+    }
+}
+module.exports = command
