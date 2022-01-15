32fdaee2bbc95a162d18ce816be1186aae584b36
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2022-01-15 10:00:44 +0530

    Update getjids.js

--- commands/getjids.js
+++ commands/getjids.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-
+const path = require('path');
 class command {
     constructor(){
 this.command = 'grpjids' // command goes here
 this.aliases = ['gj']
     }
     run = async(m)=>{
-     const jids = fs.readFileSync('../../grpjids.json');
+     const jids = fs.readFileSync(path.join(__dirname,'grpjids.json'));
 
        m.reply(JSON.stringify(JSON.parse(jids)))
     }
