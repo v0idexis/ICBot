69074f41b6a6785c0c65510a4b7b9ca42a744014
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2022-01-15 10:19:37 +0530

    Update enable.js

--- commands/enable.js
+++ commands/enable.js
@@ -8,12 +8,12 @@ this.command = 'enable' // command goes here
     run = async(M)=>{
 const value = fs.readFileSync(path.join(__dirname,'..','grpjids.json'));
           const obj = JSON.parse(value);
-          obj.push(from);
+          obj.push(M.from);
           fs.writeFileSync("grpjids.json", JSON.stringify(obj));
-          reply("successfully enabled");
+          M.reply("successfully enabled");
 
 
-       M.reply(`hi ${M.sender.username}`)
+    //   M.reply(`hi ${M.sender.username}`)
     }
 }
 module.exports = Command
